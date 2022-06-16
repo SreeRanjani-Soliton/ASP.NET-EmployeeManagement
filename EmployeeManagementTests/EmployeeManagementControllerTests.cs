c7585f689b7fef3e216711376b718c71aed1f5e3
SreeRanjani <sreeranjani@solitontech.com>
2022-06-16 12:22:48 +0530

    Added test cases for update and error

--- EmployeeManagementTests/EmployeeManagementControllerTests.cs
+++ EmployeeManagementTests/EmployeeManagementControllerTests.cs
@@ -12,6 +12,7 @@ namespace EmployeeManagementTests
 {
     public class EmployeeManagementControllerTests
     {
+        #region Read
         [Fact]
         public async void GetAllEmployees_CallGet_Success_ReturnAllEmployee()
         {
@@ -70,6 +71,27 @@ namespace EmployeeManagementTests
             Should().BeEquivalentTo(expectedResult);
         }
 
+        [Fact]
+        public async void GetSingleEmployee_CallGet_Success_ReturnNotFo()
+        {
+            //Arrange
+            var expectedResult = new ServiceResponse<GetEmployeeDTO>()
+            {
+                Data = null
+            };
+
+            var mockEmpService = new Mock<IEmployeeManagerService>();
+            mockEmpService.Setup(emp => emp.GetEmployeeById(0)).ReturnsAsync(expectedResult);
+            var EmpController = new EmployeeManagerController(mockEmpService.Object);
+            //Act
+            var actualResult = await EmpController.GetSingleEmployeeAsync(0);
+            //Assert
+            (actualResult.Result as NotFoundObjectResult).Value.
+            Should().BeEquivalentTo(expectedResult);
+        }
+        #endregion
+
+        #region Create
         [Fact]
         public async void AddEmployee_CallPost_Success_ReturnAllEmployee()
         {
@@ -152,6 +174,36 @@ namespace EmployeeManagementTests
         }
 
         [Fact]
+        public async void AddEmployee_CallPost_Success_ReturnNotFound()
+        {
+            //Arrange
+            var newSampleEmp = new AddEmployeeDTO()
+            {
+                Name = "Shiva",
+                MailId = "shiva@gmail.com",
+                JobTitle = EmployeeManagement.Models.JobTitle.ProjectEngineer,
+                Mission = EmployeeManagement.Models.Mission.GUI,
+                ProjectName = "ABC",
+                ReportsTo = "Tim"
+            };
+
+            var expectedResult = new ServiceResponse<List<GetEmployeeDTO>>();
+            expectedResult.Data = null;
+
+
+            var mockEmpService = new Mock<IEmployeeManagerService>();
+            mockEmpService.Setup(emp => emp.AddEmployee(newSampleEmp)).ReturnsAsync(expectedResult);
+            var EmpController = new EmployeeManagerController(mockEmpService.Object);
+            //Act
+            var actualResult = await EmpController.AddEmployeeAsync(newSampleEmp);
+            //Assert
+            (actualResult.Result as OkObjectResult).Value.
+            Should().BeEquivalentTo(expectedResult);
+        }
+        #endregion
+
+        #region Delete
+        [Fact]
         public async void DeleteEmployee_CallDelete_Success_ReturnAllEmployee()
         {
             //Arrange
@@ -191,6 +243,119 @@ namespace EmployeeManagementTests
             (actualResult.Result as OkObjectResult).Value.
             Should().BeEquivalentTo(expectedResult);
         }
+
+        [Fact]
+        public async void DeleteEmployee_CallDelete_Success_ReturnNotFound()
+        {
+            //Arrange
+            var expectedResult = new ServiceResponse<List<GetEmployeeDTO>>();
+            expectedResult.Data = null;
+
+            var mockEmpService = new Mock<IEmployeeManagerService>();
+            mockEmpService.Setup(emp => emp.DeleteEmployee(0)).ReturnsAsync(expectedResult);
+            var EmpController = new EmployeeManagerController(mockEmpService.Object);
+            //Act
+            var actualResult = await EmpController.DeleteEmployeeAsync(0);
+            //Assert
+            (actualResult.Result as OkObjectResult).Value.
+            Should().BeEquivalentTo(expectedResult);
+        }
+        #endregion
+
+        #region Update
+        [Fact]
+        public async void UpdateEmployee_CallPut_Success_ReturnUpdatedEmployee()
+        {
+            //Arrange
+            var expectedResult = new ServiceResponse<GetEmployeeDTO>();
+            var sampleData = GetSampleData()[0];
+            sampleData.JobTitle = JobTitle.ProjectManager;
+            sampleData.Mission = Mission.GUI;
+            expectedResult.Data = sampleData;
+
+            var updatedEmp = new UpdateEmployeeDTO()
+            {
+                Name = sampleData.Name,
+                MailId = sampleData.MailId,
+                JobTitle = sampleData.JobTitle,
+                Mission = sampleData.Mission,
+                ProjectName = sampleData.ProjectName,
+                ReportsTo = sampleData.ReportsTo
+            };
+
+            var mockEmpService = new Mock<IEmployeeManagerService>();
+            mockEmpService.Setup(emp => emp.UpdateEmployee(0, updatedEmp)).ReturnsAsync(expectedResult);
+            var EmpController = new EmployeeManagerController(mockEmpService.Object);
+            //Act
+            var actualResult = await EmpController.UpdateEmployeeAsync(0, updatedEmp);
+            //Assert
+            (actualResult.Result as OkObjectResult).Value.
+            Should().BeEquivalentTo(expectedResult);
+        }
+
+        [Fact]
+        public async void UpdateEmployee_CallPut_Failure_ReturnEmployeeWithoutUpdate()
+        {
+            //Arrange
+            var expectedEmp = new ServiceResponse<GetEmployeeDTO>();
+            var sampleData = GetSampleData()[0];
+            sampleData.JobTitle = JobTitle.ProjectManager;
+            sampleData.Mission = Mission.GUI;
+            expectedEmp.Data = sampleData;
+
+            var updatedEmp = new UpdateEmployeeDTO()
+            {
+                Name = sampleData.Name,
+                MailId = sampleData.MailId,
+                JobTitle = sampleData.JobTitle,
+                Mission = sampleData.Mission,
+                ProjectName = sampleData.ProjectName,
+                ReportsTo = sampleData.ReportsTo
+            };
+
+            var mockEmp = new ServiceResponse<GetEmployeeDTO>();
+            mockEmp.Data = GetSampleData()[0];
+
+            var mockEmpService = new Mock<IEmployeeManagerService>();
+            mockEmpService.Setup(emp => emp.UpdateEmployee(0, updatedEmp)).ReturnsAsync(mockEmp);
+            var EmpController = new EmployeeManagerController(mockEmpService.Object);
+            //Act
+            var actualResult = await EmpController.UpdateEmployeeAsync(0, updatedEmp);
+            //Assert
+            (actualResult.Result as OkObjectResult).Value.
+            Should().BeEquivalentTo(expectedEmp);
+        }
+
+        [Fact]
+        public async void UpdateEmployee_CallPut_Success_ReturnNotFound()
+        {
+            //Arrange
+            var expectedResult = new ServiceResponse<GetEmployeeDTO>();
+            expectedResult.Data = null;
+
+            var sampleData = GetSampleData()[0];
+            sampleData.JobTitle = JobTitle.ProjectManager;
+            sampleData.Mission = Mission.GUI;
+            var updatedEmp = new UpdateEmployeeDTO()
+            {
+                Name = sampleData.Name,
+                MailId = sampleData.MailId,
+                JobTitle = sampleData.JobTitle,
+                Mission = sampleData.Mission,
+                ProjectName = sampleData.ProjectName,
+                ReportsTo = sampleData.ReportsTo
+            };
+
+            var mockEmpService = new Mock<IEmployeeManagerService>();
+            mockEmpService.Setup(emp => emp.UpdateEmployee(0, updatedEmp)).ReturnsAsync(expectedResult);
+            var EmpController = new EmployeeManagerController(mockEmpService.Object);
+            //Act
+            var actualResult = await EmpController.UpdateEmployeeAsync(0, updatedEmp);
+            //Assert
+            (actualResult.Result as NotFoundObjectResult).Value.
+            Should().BeEquivalentTo(expectedResult);
+        }
+        #endregion
 
         #region HelperMethods
         private List<GetEmployeeDTO> GetSampleData()
